62da205a352eabf339ff99b1f8d6b18fb3425b6d
Maicon de Farias <maicondefarias@gmail.com>
2023-07-28 20:35:32 +0100

    New class for test

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,29 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+                
+        /* 
+         * For american pattern, you must type 1000 for a thousand (no dot or comma).
+         * If you want to write decimal numbers, you must to use a dot to separate
+         * the unit from decimal number (after comma).
+         * 
+         * E.g.: 2.500 WRONG! (it's two comma five)
+         *       2500 Correct! (it's two thousand five hundred)
+         * 
+         * E.g.2: 1500,5 WRONG! (No commas, you dummie!)
+         *        1500.5 Correct! (Yes! It's one thousand five hundred dot five!)
+         */
+
+        byte age = 123;
+        short year = 2023;
+        int zipCode = 12309360; //If it starts in 0, maybe you must use a String data
+        long vatNumber = 240278000L; //If it starts in 0, maybe you must use other type.
+                                     // Java needs the letter L in the final a Long type to know that it's realy long!
+        float pi = 3.14F; //After a float number YOU MUST write the letter F
+        final double WAGE = 1275.33; //the constant starts with "final" word and we write using CAPSLOCK
+
+        short shortNumber = 1;
+        int normalNumber = shortNumber;
+        short shortNumber2 = (short) normalNumber; // Java doesn't accept add a integer in a short type variable. So we must convert to short type. This convertion is called "casting"
+
     }
 }
