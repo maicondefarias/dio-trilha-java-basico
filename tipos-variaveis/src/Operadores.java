bd26d371d3680a56b7549fc4c5dbab0c36f37e1a
Maicon de Farias <maicondefarias@gmail.com>
2023-07-31 22:10:51 +0100

    Boolean

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,7 +1,13 @@
 public class Operadores{
 
-    public static void public static void main(String[] args) {
-        
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 < numero2;
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
     }
 
 }
