fb2c20d0ca4570687345d39f8cc2dd1068c24045
Maicon de Farias <maicondefarias@gmail.com>
2023-08-15 19:42:48 +0100

    Configurações json

---
+++ terminal-argumentos/src/MinhaClasse.java
@@ -0,0 +1,10 @@
+public class MinhaClasse{
+    public static void main(String[] args) {
+
+        int numero1 = -20;
+        int numero2 = 10;
+
+        System.out.println(numero1+numero2);
+    }
+
+}
