795e4ab885057cf16cbf5ef9ea2b78c319069b24
Maicon de Farias <maicondefarias@gmail.com>
2023-08-15 11:15:16 +0100

    Learning Java Commetaries

---
+++ java-terminal/calculadora/src/Comentarios.java
@@ -0,0 +1,20 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        //Comentário em uma única linha com informação a nível de usuário
+        
+        /*
+         *Comentário mais detalhado 
+         *quando for necessário
+         */
+    }
+    /**
+     * Estas duas estrelinhas acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação
+     * e tem se ser colocado fora do método
+     */
+    public void metodo(){
+        
+    }
+}
